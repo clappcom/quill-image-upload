f044cd17244d526935a76371b30b7b88ec9bc8eb
dsge <gerx03@gmail.com>
2018-11-30 12:31:12 +0100

    trial and error #1

--- bin/release.sh
+++ bin/release.sh
@@ -8,6 +8,7 @@ cp $dir/src/es5-wrapper.js $dir/image-upload.min.js
 # Compile to ES5
 js=$(node $dir/node_modules/babel-cli/bin/babel.js $dir/src/image-upload.js --presets=es2015 | $dir/node_modules/uglify-js/bin/uglifyjs -m)
 echo $js > tmp.js
+echo $js > image-upload.js
 # Wrap
 sed -i -e '/MINIFIED_JS/r tmp.js' -e '/MINIFIED_JS/d' $dir/image-upload.min.js
 rm tmp.js
